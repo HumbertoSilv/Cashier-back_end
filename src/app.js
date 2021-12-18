b30c7a9eaa59d1fa9e35e7cdc8821b5985c9f4f8
Humberto <matheushumberto520@gmail.com>
2021-12-18 09:37:46 -0400

    Refac: created controller file and refactored structure

--- src/app.js
+++ src/app.js
@@ -1,43 +1,18 @@
-import express, { response } from "express";
-import availableNotes from "./config/availableNotes";
+import express from "express";
 import { cashierSchema } from './models/schema/schemaCashier';
 import { validateBody, checkIfHasNotes } from './middleware/middlewares';
+import cashierFunction from  './controller/cashierController';
+
 
 const app = express();
 
 app.use(express.json());
 app.use('/cashier', validateBody(cashierSchema), checkIfHasNotes);
 
+app.post('/cashier', cashierFunction);
 
-app.post('/cashier', (req, res) => {
-    let { thing } = req.body;
-    const changeInNotes = {};
+export default app;
 
-    for (let i = 0; i < availableNotes.length; i++) {
-        let noteValue = Object.keys(availableNotes[i]);
-        let numberOfNotes = Math.floor(thing / parseInt(noteValue));
-
-        if(numberOfNotes != 0 ){
-            changeInNotes[noteValue] = numberOfNotes;
-            thing -= numberOfNotes * noteValue;            
-        };
-
-        if(availableNotes[i][noteValue] > numberOfNotes) {
-            availableNotes[i][noteValue] -= numberOfNotes;
-        } else{
-            return res.status(400).send({Error: 'Not enough grades.'});
-        };
-    };
-    console.log(availableNotes);
-    response = {
-        ...req.body,
-        notes: changeInNotes
-    };
-
-    res.status(200).send(response);
-
-});
-
-app.listen('3002', () => {
-    console.log('Running at http://localhost:3000/');
-});
+// app.listen('3002', () => {
+//     console.log('Running at http://localhost:3000/');
+// });
---
+++ src/controller/cashierController.js
@@ -0,0 +1,32 @@
+import availableNotes from "../config/availableNotes";
+
+const cashierFunction = (req, res) => {
+    let { thing } = req.body;
+    const changeInNotes = {};
+
+    for (let i = 0; i < availableNotes.length; i++) {
+        let noteValue = Object.keys(availableNotes[i]);
+        let numberOfNotes = Math.floor(thing / parseInt(noteValue));
+
+        if(numberOfNotes != 0 ){
+            changeInNotes[noteValue] = numberOfNotes;
+            thing -= numberOfNotes * noteValue;            
+        };
+
+        if(availableNotes[i][noteValue] > numberOfNotes) {
+            availableNotes[i][noteValue] -= numberOfNotes;
+        } else{
+            return res.status(400).send({Error: 'Not enough grades.'});
+        };
+    };
+    console.log(availableNotes);
+    const response = {
+        ...req.body,
+        notes: changeInNotes
+    };
+
+    res.status(200).send(response);
+
+};
+
+export default cashierFunction;
